0d252cb7773d5e76ea1588f67c5bd87c4701741e
Chaitanya Gomaskar <chaitanyagomaskar25@navgurukul.org>
2026-08-21 16:32:05 +0530

    Day 15 doe

---
+++ DSA_Day_15.py
@@ -0,0 +1,56 @@
+# Ques1: Write a Python program that takes a number n and prints a square pattern of * with n rows and n columns.
+
+# Input: 4
+# Output:
+# ****
+# ****
+# ****
+# ****
+
+# n = int(input())
+# for i in range(n):
+#     ans = ""
+#     for j in range(n):
+#         ans+="*"
+#     print(ans)
+
+
+# =================================
+# Ques2: Write a Python program that takes a number n and prints a right-angled triangle pattern.
+
+# Input: 5
+# Output:
+# *
+# **
+# ***
+# ****
+# *****
+
+# n = int(input())
+# for i in range(1,n+1):
+#     ans = ""
+#     for j in range(i):
+#         ans+="*"
+#     print(ans)
+
+# ===============================
+
+# Ques3: Write a Python program that takes a number n and prints the following number pattern using nested loops.
+
+# Input: 5
+# Output:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# n = int(input())
+
+# for i in range(1, n+1):
+#     ans = ""
+#     for j in range(1,i+1):
+#         ans+=f"{j}"
+#     print(ans)
+
+# ========================
